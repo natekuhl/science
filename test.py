9ffd488a06dc41a77a2394e61196d4afa3c1751a
Nate Kuhl <nkuhl@mysidewalk.com>
2017-07-18 15:07:23 -0500

    Include modified visualize python file

---
+++ test.py
@@ -0,0 +1,13 @@
+import graphviz as gv
+
+j=0
+
+g1 = gv.Graph(format='png')
+g1.node('A', 'happy little node')
+g1.node('B')
+g1.node('{}'.format(j))
+g1.edge('A' , 'B')
+
+
+
+print(g1.source)
--- visualize-dot_temporal.py
+++ visualize-dot_temporal.py
@@ -4,8 +4,8 @@ import argparse
 import os
 from path import Path
 import textwrap
-import pygraphviz as pgv
-
+import graphviz as gv
+import re
 
 from configobj import ConfigObj
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
@@ -54,19 +54,30 @@ def run():
     engine = create_engine(get_db_connection_string())
     system_names = get_system_names(engine)
     def make_digraph_adv():
-        G = pgv.AGraph()
+        G = gv.Graph(format='svg')
+        j = 1
         for name in system_names:
+            n = name.split("__")
             i = 0
-            while i < (len(name.split("__"))-1):
-                if i+1 == (len(name.split("__"))-1):
-                    G.add_node(name)
-                    G.add_edge(name.split("__")[i],name)
-                    i += 1        
+            while i <= (len(name.split("__"))-1):
+                if i == 0:
+                    G.node('1','{}'.format(n[i]))
+                    i += 1
+                    j += 1
                 else:
-                    G.add_node(name.split("__")[i])
-                    G.add_edge(name.split("__")[i],name.split("__")[i+1])
-                    i += 1        
-        G.write("{}".format(args.dot_file_name))
+                    if i == 1:
+                        G.node('{}'.format(j),'{}'.format(n[i]))
+                        G.edge('1','{}'.format(j))
+                        i += 1
+                        j += 1
+                    else:
+                        G.node('{}'.format(j),'{}'.format(n[i]))
+                        G.edge('{}'.format(j-1),'{}'.format(j))
+                        i += 1
+                        j += 1
+        #filename = G.render(filename="{}".format(args.dot_file_name))
+        #print filename
+        G.render('img/{}'.format(args.dot_file_name))   
     make_digraph_adv()
     #unflatten -l100 -f args.dot_file_name > args.dot_file_name
     #dot -Gdpi=3000 -Gsize=9,15\! -Tpng args.dot_file_name -o args.png_name 
@@ -75,3 +86,4 @@ def run():
 
 if __name__ == '__main__':
     run()
+
